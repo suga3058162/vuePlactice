3fb73868bc8fa670299e9f8b1744ed0aa3c8e543
tomita <tomita@itpropartners.jp>
2018-08-13 13:32:44 +0900

    プロパティの追加

--- main.js
+++ main.js
@@ -8,24 +8,10 @@ var app = new Vue({
         { id: 3, name: 'ドラゴン', hp: 500 }
         ]
     },
-    methods: {
-        // 削除ボタンをクリックしたときのハンドラ
-        doRemove: function(index) {
-            // 受け取ったインデックスの位置から1個要素を削除
-            this.list.splice(index, 1)
-        },
-        // 追加ボタンをクリックしたときのハンドラ
-        doAdd: function() {
-            // リスト内で一番大きいIDを取得
-            var max = this.list.reduce(function(a, b) {
-                return a > b.id ? a : b.id
-            },　0)
-            // 新しいモンスターをリストに追加
-            this.list.push({
-                id: max + 1, // 現在の最大のIDにプラス1してuniqueなIDを作成
-                name: this.name, // 現在のフォームの入力値
-                hp: 500
-            })
-        }
+    created: function() {
+        // すべての要素にactiveプロパティを追加したい
+        this.list.forEach(function(item) {
+            this.$set(item, 'active', false)
+        }, this)
     }
 })
