46fa04ff6efc6489cc4d9f1f96199e2905b08350
tomita <tomita@itpropartners.jp>
2018-08-13 14:34:12 +0900

    $refs

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
-        name: 'キマイラ',
-        list: [
-        { id: 1, name: 'スライム', hp: 100 },
-        { id: 2, name: 'ゴブリン', hp: 200 },
-        { id: 3, name: 'ドラゴン', hp: 500 }
-        ]
+      show: true
     },
-    created: function() {
-        // すべての要素にactiveプロパティを追加したい
-        this.list.forEach(function(item) {
-            this.$set(item, 'active', false)
-        }, this)
+    methods: {
+      handleClick() {
+        var count = this.$refs.count
+        if (count) {
+          count.innerText = parseInt(count.innerText, 10) + 1
+        }
+      }
     }
-})
+  })
