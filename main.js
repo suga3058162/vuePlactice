06c3e4b51146cb928fc6425a76b50aa48b19fd62
tomita <tomita@itpropartners.jp>
2018-08-13 11:09:01 +0900

    SVGのデータバインディング

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        item: {
-            id: 1,
-            src: 'item1.png',
-            alt: '商品サムネイル',
-            width: 100,
-            height: 100
-        }
+      radius: 50
     }
 })
